9131c3f77c5e623c6e965d950533a44401093202
Kirillova Olesya <olesyak-85@mail.ru>
2021-10-22 23:32:16 +0300

    Added TestV1

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts(){
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://0.0.0.0:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(300)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
+    }
+}
