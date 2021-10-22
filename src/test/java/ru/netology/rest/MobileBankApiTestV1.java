1da39184dc9aa3e93d8a67056510b22cbad3c8a7
Kirillova Olesya <olesyak-85@mail.ru>
2021-10-22 23:37:29 +0300

    modified TestV1

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -17,7 +17,7 @@ public class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(300)
+                .statusCode(200)
                 // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
         ;
